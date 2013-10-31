0c21764e2b2c58ccb68f5c6574acc18cf5ae518d
Antoine Guigan <antoine@akeneo.com>
2013-10-31 14:00:24 +0100

    Initial import

---
+++ DataFixtures/ORM/LoadImportProfiles.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Pim\Bundle\IcecatDemoBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Symfony\Bridge\Doctrine\Tests\Fixtures\ContainerAwareFixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Oro\Bundle\BatchBundle\Entity\JobInstance;
+
+/**
+ *
+ * @author    Antoine Guigan <antoine@akeneo.com>
+ * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class LoadImportProfiles extends ContainerAwareFixture implements FixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $importDir = $this->container->getParameter('pim_demo.import_dir');
+        $this->loadJobInstance(
+            $manager,
+            'category_import',
+            'initial_category_import',
+            'Initial category import',
+            sprintf('%s/%s', $importDir, 'categories.csv')
+        );
+        $productFiles = glob(sprintf('%s/products/*.csv', $importDir));
+        foreach ($productFiles as $file) {
+            $jobInstanceCode = basename($file, '.csv');
+            $this->loadJobInstance(
+                $manager,
+                'product_import',
+                sprintf('initial_product_import_%s', $jobInstanceCode),
+                sprintf('Initial product import %s', $jobInstanceCode),
+                $file
+            );
+        }
+        $manager->flush();
+    }
+    protected function loadJobInstance(ObjectManager $manager, $alias, $code, $label, $file, $channel = null)
+    {
+        $jobInstance = new JobInstance('Akeneo CSV Connector', 'import', $alias);
+        $connectorRegistry = $this->container->get('oro_batch.connectors');
+        $job = $connectorRegistry->getJob($jobInstance);
+        $steps = $job->getSteps();
+        $step = array_shift($steps);
+        $step->getReader()->setFilePath($file);
+        $jobInstance
+            ->setCode($code)
+            ->setLabel($label)
+            ->setJob($job);
+        $manager->persist($jobInstance);
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+Akeneo PIM Icecat Demo Bundle
+=============================
+
+This bundle contains real world data coming from the Icecat databases.
+
+To install the demo data, please use the following commands :
+
+    sh install.sh db
+    php app/console pim:icecat-demo:install --env=prod
+
+
+For more information about Icecat, please see http://icecat.biz

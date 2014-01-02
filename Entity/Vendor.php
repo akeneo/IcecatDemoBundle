0a7204d08224d860a93b0b2699c5a4e643bd054e
Antoine Guigan <antoine@akeneo.com>
2014-01-02 14:23:33 +0100

    Moved ProductValue to Entity namespace

--- Entity/Vendor.php
+++ Entity/Vendor.php
@@ -2,9 +2,6 @@
 
 namespace Pim\Bundle\IcecatDemoBundle\Entity;
 
-use DateTime;
-use Doctrine\ORM\Mapping as ORM;
-use Gedmo\Mapping\Annotation as Gedmo;
 use Pim\Bundle\CatalogBundle\Model\ReferableInterface;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Validator\Constraints as Assert;
@@ -15,28 +12,18 @@ use Symfony\Component\Validator\Constraints as Assert;
  * @author    Antoine Guigan <antoine@akeneo.com>
  * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- * @ORM\Entity(repositoryClass="Pim\Bundle\CatalogBundle\Entity\Repository\ReferableEntityRepository")
- * @ORM\Table(
- *     name="pim_icecatdemo_vendor",
- *     uniqueConstraints={@ORM\UniqueConstraint(name="pim_icecatdemo_vendor_code", columns={"code"})}
- * )
  * @UniqueEntity(fields="code", message="This code is already taken")
  */
 class Vendor implements ReferableInterface
 {
     /**
      * @var integer $id
-     *
-     * @ORM\Column(name="id", type="integer")
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
 
     /**
      * @var string $code
      *
-     * @ORM\Column(name="code", type="string", length=100)
      * @Assert\Regex(pattern="/^[a-zA-Z0-9_]+$/")
      * @Assert\Length(max=100, min=1)
      */
@@ -45,31 +32,22 @@ class Vendor implements ReferableInterface
     /**
      * @var string $label
      *
-     * @ORM\Column(name="label", type="string", length=250, nullable=false)
      * @Assert\Length(max=250, min=1)
      */
     protected $label;
 
     /**
      * @var string
-     * 
-     * @ORM\Column
      */
     protected $responsible;
 
     /**
      * @var datetime $created
-     *
-     * @Gedmo\Timestampable
-     * @ORM\Column(name="created", type="datetime")
      */
     protected $created;
 
     /**
      * @var datetime $updated
-     *
-     * @Gedmo\Timestampable
-     * @ORM\Column(name="updated", type="datetime")
      */
     protected $updated;
 
